3fa3816aff52910bd82c57931d69d3cec9f161c6
Maimuna <maimunaibrahim808@gmail.com>
2024-11-12 02:01:58 +0100

    new action

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,8 +1,23 @@
-import Header from './components/Header';
+import React from 'react'
+import JSXRules from './components/JSXRules'
+
 const App = () => {
- return <div>
-  <Header />
- </div>
- 
-};
-export default App;
+const myName = 'John';
+const multiply= (a, b) => a + b 
+const specialClass = 'special'
+
+
+  return (
+  
+    <section>
+        <p>2 + 2 = {2 + 2}</p>
+        <h1>{myName}</h1>
+        <p>My friends List: {[ 'Alex', 'John', 'Waheed']}</p>
+        <p>2 * 2 = { multiply (2, 10)}</p>
+        <p className={special}>This is special </p>
+    </section>
+  
+  )
+}
+
+export default App
