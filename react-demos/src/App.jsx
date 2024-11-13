7ac87891f803bc9e143d0c6816a77f4313cf9f76
Maimuna <maimunaibrahim808@gmail.com>
2024-11-13 08:47:32 +0100

    new action

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,28 +1,35 @@
 import React from "react";
+const Cart = () => {
+const items = [
+  "Iphone", "Powerbank", "Shoes", "Car", "Hoddie"
+];
+
+
+
+ return <div>
+  <h1>Cart </h1>
+  {items.length > 0 && <h2>You have {items.length}
+    items in your Cart</h2>}
+
+    <ul>
+      <h4>Products</h4>
+
+      {items.map(item => (
+        <li key={Math.random}>{item}</li>
+      ))}
+    </ul>
+ </div>
+}
+
 
 const App = () => {
   return (
-    <User
-      name="Maimunah"
-      age={25}
-      isMarried={true}
-      hobbies={["reading", "coding", "traveling"]}
-      img="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80"
-    />
-  );
-};
-
-const User = (props) => {
-  console.log(props);
-  return (
     <div>
-      <img src={props.img} alt="profile" width={200} />
-      <h1>Name: {props.name}</h1>
-      <h2>Age: {props.age}</h2>
-      <h2>Is Married: {props.isMarried ? "Yes" : "No"}</h2>
-      <h2>Hobbies:{props.hobbies}</h2>
+<Cart/>
+
+
     </div>
-  );
-};
+  )
+}
 
 export default App;
