577cfdf963d7c6d8f9d5de1cbf4084a10cecd909
Maimuna <maimunaibrahim808@gmail.com>
2024-11-21 00:46:46 +0100

    new

---
+++ react-demos/src/Testamonails/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Testamonials from './Testamonails'
+
+const App = () => {
+    
+  return (
+    <div>
+<Testamonials/>
+    </div>
+  )
+}
+
+export default App
--- react-demos/src/main.jsx
+++ react-demos/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './Hidden/App'
+import App from './Accordion/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
