ab91f2a770d7bb1967d259a05b56238ff1b42ae3
Maimuna <maimunaibrahim808@gmail.com>
2024-11-20 04:03:40 +0100

    new

---
+++ react-demos/src/Hidden/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Hidden from './Hidden'
+
+const App = () => {
+  return (
+    <div>
+      <Hidden/>  
+    </div>
+  )
+}
+
+export default App
--- react-demos/src/main.jsx
+++ react-demos/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './Calculator/App'
+import App from './Hidden/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
