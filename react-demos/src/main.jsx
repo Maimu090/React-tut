6d5d38e4962f326b9447cc0f8fba1cd41d7948f2
Maimuna <maimunaibrahim808@gmail.com>
2024-11-21 09:13:47 +0100

    new

--- react-demos/src/main.jsx
+++ react-demos/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './Accordion/App'
+import App from './Form/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
